082ed92c8e4c6d770a0a0ea92cb8568afbf7158a
Russell <blahs21@gmail.com>
2015-07-01 12:32:59 -0700

    added cut contigs to results

--- bashPractice/repeatfinder/myrepeatfinder.sh
+++ bashPractice/repeatfinder/myrepeatfinder.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# used for contig files
+# repeats are unwanted
+
 fafile=shortCAP3TestFile
 results=myrepeats.txt
 ranges="1-50 100-150 200-250"
@@ -44,10 +47,32 @@ done
 # progress is found with wc -l tempfile
 rm -f $tempfile
 
+
+# add in the actual cut repeated sequences
+tempResults=tempmyrepeats396
+> $tempResults
+resultlen=($(wc -l $results))
+i=1
+while [ $i -le $resultlen ]; do
+  repeatline=$(awk -F'\t' -v line="$i" 'NR==line {print $0}' $results)
+  echo $repeatline >> $tempResults
+  linenum=$(echo $repeatline | awk -F' ' '{print $2}')
+  # echo $linenum
+  cutsection=$(echo $repeatline | awk -F' ' '{print $3}')
+  # echo $cutsection
+  contig=$(awk -F'\t' -v line="$linenum" 'NR==line {print $0}' $fafile | cut -c $cutsection)
+  # echo $contig
+  echo $contig >> $tempResults
+  let i+=1
+done
+
+cp $tempResults $results
+rm -f $tempResults
+
 date +"%m/%d/%Y %H:%M:%S ${0##*/} finished"
 
 
 # things to consider:
 # agrep
-# ignore 'N's
 # go through, find the actual repeated sequences
+# why didn't i add the sequence when doing the results, instead of at the end??
