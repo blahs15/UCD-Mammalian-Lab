0b48c46501a21c6222e2b9f11fe3bfdc3e812afd
Russell <blahs21@gmail.com>
2015-07-01 10:18:55 -0700

    ignore Ns in myrepeatfinder.sh

--- bashPractice/repeatfinder/myrepeatfinder.sh
+++ bashPractice/repeatfinder/myrepeatfinder.sh
@@ -19,10 +19,20 @@ do
 
   for range in $ranges; do
     # echo $range
+    
+    cutStr=$(echo $contig | cut -c $range)
+
+    # masking replaces repeated strings we don't care about with N's
+    strlen=$( echo $cutStr | wc -c )
+    # if [ cutStr == $(printf 'N%.s' {1..$strlen}) ]; then # probably slower
+    if [ $cutStr == $(head -c $strlen < /dev/zero | tr '\0' 'N') ]; then
+      echo "skipping line ${i}, range $range" # testing whether works
+      continue
+    fi
 
     # depending on computer, following line may not work
-    # repeatnum=$(echo $contig | cut -c $range | grep -f - $tempfile | wc -l)
-    repeatnum=$(echo $contig | cut -c $range | grep -f /dev/stdin $tempfile | wc -l)
+    # repeatnum=$( echo $cutStr | grep -f - $tempfile | wc -l)
+    repeatnum=$( echo $cutStr | grep -f /dev/stdin $tempfile | wc -l)
     if [ $repeatnum -gt 0 ]; then
       echo -e "line ${i}\t${range}\t${repeatnum}" >> $results # print results
     fi
@@ -31,6 +41,13 @@ do
   let i+=2 # every other line
 done
 
-# rm -f $tempfile
+# progress is found with wc -l tempfile
+rm -f $tempfile
 
 date +"%m/%d/%Y %H:%M:%S ${0##*/} finished"
+
+
+# things to consider:
+# agrep
+# ignore 'N's
+# go through, find the actual repeated sequences
